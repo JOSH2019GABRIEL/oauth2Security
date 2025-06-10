55f901e25a26fb398dc6ca531044615be21058f0
joshuagabriel-datafi <joshua.gabriel@thepalladiumgroup.com>
2025-06-10 10:02:37 +0100

    initial PR

---
+++ src/test/java/com/joshcode/Security/Demo/SecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshcode.Security.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
